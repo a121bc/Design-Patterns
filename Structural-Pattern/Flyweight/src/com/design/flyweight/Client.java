2483e720d1500f86bd98d4aead68dd6b5267cb14
a121bc <a121bc@163.com>
2019-10-28 22:10:18 +0800

    享元模式 学习实例

---
+++ Structural-Pattern/Flyweight/src/com/design/flyweight/Client.java
@@ -0,0 +1,41 @@
+package com.design.flyweight;
+
+import com.design.flyweight.entity.Flyweight;
+import com.design.flyweight.factory.FlyweightFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /*FlyweightFactory factory = new FlyweightFactory();
+        Flyweight fly = factory.factory(new Character('a'));
+        fly.operation("First Call");
+
+        fly = factory.factory(new Character('b'));
+        fly.operation("Second Call");
+
+        fly = factory.factory(new Character('a'));
+        fly.operation("Third Call");*/
+        List<Character> compositeState = new ArrayList<Character>();
+        compositeState.add('a');
+        compositeState.add('b');
+        compositeState.add('c');
+        compositeState.add('a');
+        compositeState.add('b');
+
+        FlyweightFactory flyFactory = new FlyweightFactory();
+        Flyweight compositeFly1 = flyFactory.factory(compositeState);
+        Flyweight compositeFly2 = flyFactory.factory(compositeState);
+        compositeFly1.operation("Composite Call");
+
+        System.out.println("---------------------------------");
+        System.out.println("复合享元模式是否可以共享对象：" + (compositeFly1 == compositeFly2));
+
+        Character state = 'a';
+        Flyweight fly1 = flyFactory.factory(state);
+        Flyweight fly2 = flyFactory.factory(state);
+        System.out.println("单纯享元模式是否可以共享对象：" + (fly1 == fly2));
+    }
+}

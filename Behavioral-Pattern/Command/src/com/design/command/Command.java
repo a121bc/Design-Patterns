fa18de2d0ff2ef04ead9dcbab536de45f616a68f
a121bc <a121bc@163.com>
2019-11-02 11:06:01 +0800

    命令模式 学习实例

---
+++ Behavioral-Pattern/Command/src/com/design/command/Command.java
@@ -0,0 +1,11 @@
+package com.design.command;
+
+/**
+ * 抽象命令角色类
+ */
+public interface Command {
+    /**
+     * 执行方法
+     */
+    void execute();
+}
---
+++ Behavioral-Pattern/Command/src/com/design/command/ConcreteCommand.java
@@ -0,0 +1,19 @@
+package com.design.command;
+
+/**
+ * 具体命令角色类
+ */
+public class ConcreteCommand implements Command {
+    //持有相应的接收者对象
+    private Receiver receiver = null;
+    /**
+     * 构造方法
+     */
+    public ConcreteCommand(Receiver receiver){
+        this.receiver = receiver;
+    }
+    @Override
+    public void execute() {
+        receiver.action();
+    }
+}
